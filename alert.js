897c40b7b8097f8639d4bd6bf83552453fc3464c
truespring <true_spring_@naver.com>
2021-06-10 19:37:45 +0900

    잡힐 확률 추가

--- alert.js
+++ alert.js
@@ -1,3 +1,6 @@
+var catchRatio = undefined;
+var exp = undefined;
+
 swal("A wild Pikachu appeared! What do you want to do?", {
     buttons: {
         cancel: "Run away!",
@@ -12,11 +15,16 @@ swal("A wild Pikachu appeared! What do you want to do?", {
     switch (value) {
     
         case "defeat":
-        swal("Pikachu fainted! You gained 500 XP!");
+            swal("Pikachu fainted! You gained 500 XP!");
+            exp += 500;
         break;
-    
         case "catch":
-        swal("Gotcha!", "Pikachu was caught!", "success");
+            catchRatio = Math.floor(Math.random() * 100);
+            if (catchRatio >= 10) {
+                swal("Gotcha!", "Pikachu was caught!", "success");
+            } else {
+                swal("Damn it!", "Pikachu was escape!", "warning")
+            }
         break;
     
         default:
